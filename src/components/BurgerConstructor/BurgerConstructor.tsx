6409657f0b0ad32799b87de460e8f4a797f930c6
Maxim Pozharskiy <maxim.pozharskiy@deleteagency.com>
2021-08-17 21:57:53 +0300

    feat: add burger-contstructor

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,11 +1,54 @@
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 import styles from './BurgerConstructor.module.css';
+import { data } from '../../utils/data';
 
 function BurgerConstructor() {
   return (
-      <div className="App">
-      Hello
+    <section className={`${styles.construct} mt-10`}>
+      <div className="ml-4 mt-4">
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text="Краторная булка N-200i (верх)"
+          price={20}
+          thumbnail={data[0].image}
+        />
       </div>
+      
+      <ul className={styles['list-ingredients']}>
+        {data.map(element =>
+        <div className={styles['list-item-wrap']}>
+          <DragIcon type="primary" />
+          <li key={element._id}>
+            <ConstructorElement
+              text={element.name}
+              price={element.price}
+              thumbnail={element.image}
+            />
+          </li>
+        </div>
+        )}
+      </ul>
+      <div className="ml-4">
+        <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={20}
+            thumbnail={data[0].image}
+          />
+      </div>
+      <div className={`${styles['order-wrap']} mt-10`}>
+        <div className={styles['price-wrap']}>
+          <p className="text text_type_digits-medium">610</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </section>
   );
 }
 
