b777f22408fe80b9fdd2fa3efdf13ec84710fdb5
Maxim Pozharskiy <maxim.pozharskiy@deleteagency.com>
2021-11-01 00:23:43 +0300

    add TS typethation for helpers

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const mainApiUrl = 'https://norma.nomoreparties.space/api';
-export const keyCodeEsc = 27;
-export const emailRegxep =
+export const mainApiUrl: string = 'https://norma.nomoreparties.space/api';
+export const keyCodeEsc: number = 27;
+export const emailRegxep: RegExp =
   /^(([^<>()[\]\\.,;:\s@ "]+(\.[^<>()[\]\\.,;:\s@ "]+)*)|( ".+ "))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
